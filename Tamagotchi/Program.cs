f6a32a4baa661725eda75be51af53ceac4e712d3
Frejbo <frej.hellsten@gmail.com>
2023-10-05 14:36:23 +0200

    en klassisk klass

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using Raylib_cs;
+
+Raylib.InitWindow(500, 500, "Tamagotchi");
+Raylib.SetTargetFPS(5);
+
+Tamagotchi ta = new("Erik. A");
+ta.Teach("no.");
+
+while (!Raylib.WindowShouldClose())
+{
+    ta.Tick();
+    ta.Feed();
+    ta.Hi();
+    Draw();
+}
+
+void Draw()
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BEIGE);
+
+    Raylib.DrawText("Tamagotchi", 20, 50, 24, Color.BLACK);
+
+    Raylib.EndDrawing();
+}
+
+
+// ta.Feed();
+// ta.Teach("FUCK U");
+// ta.Hi();
+
+Console.ReadLine();
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -2,30 +2,42 @@ public class Tamagotchi
 {
     private int hunger;
     private int boredom;
-    private List<string> words;
+    private List<string> words = new();
     private bool isAlive;
-    private Random generator;
+    private Random generator = new();
     public string name;
+
+    public Tamagotchi(string n) {
+        name = n;
+    }
 
     public void Feed()
     {
         // Feed() sänker Hunger
-
+        hunger--;
     }
     public void Hi()
     {
         // Hi() skriver ut ett slumpat ord från words, och anropar ReduceBoredom.
-
+        ReduceBoredom();
+        if (words.Count() > 0) {
+            System.Console.WriteLine(words[generator.Next(words.Count())]);
+        }
     }
     public void Teach(string w)
     {
         // Teach(string word) lägger till ett ord i words, och anropar ReduceBoredom.
-
+        words.Add(w);
     }
     public void Tick()
     {
         // Tick() ökar hunger och boredom, och om någon av dem kommer över 10 så blir isAlive false.
-
+        hunger++;
+        boredom++;
+        if (hunger > 10 || boredom > 10)
+        {
+            isAlive = false;
+        }
     }
     public void PrintStats()
     {
@@ -42,6 +54,6 @@ public class Tamagotchi
     private void ReduceBoredom()
     {
         // ReduceBoredom() sänker boredom.
-
+        boredom--;
     }
 }
